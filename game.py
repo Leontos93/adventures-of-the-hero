a7d7ad0a2d2c5b12f02ffbdec6f5ed60c9601cc2
Leonid <bohdanovleonid93@gmail.com>
2025-10-16 08:50:41 +0300

    create def take_damage() in class Charackter

--- game.py
+++ game.py
@@ -1,24 +1,31 @@
 class Character:
     def __init__(
-        self, name, health=100, max_health=100, atack=10, level=1, experience=0
+        self, name, health=100, max_health=100, attack=10, level=1, experience=0
     ):
         self.name = name
         self.health = health
         self.max_health = max_health
-        self.atack = atack
+        self.attack = attack
         self.level = level
         self.experience = experience
 
     def show_stats(self):
         print(f"Статистика персонажу:")
         print(
-            f"Ім'я: {self.name} Здоров'я: {self.health} Сила атаки: {self.atack} Рівень: {self.level} Досвід: {self.experience}"
+            f"Ім'я: {self.name} Здоров'я: {self.health} Сила атаки: {self.attack} Рівень: {self.level} Досвід: {self.experience}"
         )
 
     def is_alive(self):
         return self.health > 0
 
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+        print(f"{self.name} отримав {damage} шкоди! Здоров'я: {self.health}")
+
 
 hero_1 = Character("Leonid")
 hero_1.show_stats()
 print(hero_1.is_alive())
+hero_1.take_damage(5)
