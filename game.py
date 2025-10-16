0fb6b863a72cbb89521a76f0b613790a2fe9625a
Leonid <bohdanovleonid93@gmail.com>
2025-10-16 10:22:50 +0300

    add function create_enemy()

--- game.py
+++ game.py
@@ -1,3 +1,26 @@
+import random
+
+
+def create_enemy(player_level):
+    enemies = [
+        {"name": "Гоблін", "health": 30, "attack": 5, "exp": 20},
+        {"name": "Орк", "health": 50, "attack": 8, "exp": 35},
+        {"name": "Тролль", "health": 80, "attack": 12, "exp": 50},
+    ]
+
+    enemy_template = random.choice(enemies)
+
+    enemy = Character(
+        name=enemy_template["name"],
+        health=enemy_template["health"] * player_level,
+        attack=enemy_template["attack"] * player_level,
+    )
+
+    enemy.exp_reward = enemy_template["exp"] * player_level
+
+    return enemy
+
+
 class Character:
     def __init__(
         self, name, health=100, max_health=100, attack=10, level=1, experience=0
@@ -36,13 +59,21 @@ class Character:
             self.max_health += 20
             self.health = self.max_health
             self.attack += 5
-        print(f"{self.name} досяг рівня {self.level}!")
+            print(f"{self.name} досяг рівня {self.level}!")
 
 
-hero_1 = Character("Leonid")
+hero = Character("Герой")
+for i in range(3):
+    enemy = create_enemy(hero.level)
+    enemy.show_stats()
+    print(f"Нагорода: {enemy.exp_reward} досвіду\n")
+hero = Character("Герой")
+enemy = create_enemy(hero.level)
+enemy.show_stats()
+"""hero_1 = Character("Leonid")
 enemy_1 = Character("Гоблін")
 hero_1.attack_enemy(enemy_1)
 enemy_1.show_stats()
 hero_1.show_stats()
 print(hero_1.is_alive())
-hero_1.take_damage(5)
+hero_1.take_damage(5)"""
